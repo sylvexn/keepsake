e91746737a551fadcc5278f57c18391868edad9a
sylvexn <sylvexn@gmail.com>
2025-05-03 23:27:04 -0400

    database fix 2.0

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Card } from "@heroui/react";
 import { Chip } from "@heroui/react";
 import MainLayout from "../layouts/MainLayout";
